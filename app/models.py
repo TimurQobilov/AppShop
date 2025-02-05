0fd7c5c64c0d601523d4cb9588b1937a2646aa57
TimurQobilov <qobilovtemur.dev@gmail.com>
2025-02-05 21:43:44 +0500

    add main

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, TIMESTAMP, func, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 class User(Base):
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
-from app.models import User
-
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:timur910210@localhost/online_store'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:timur910210@localhost/online_store'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    isActive = db.Column(db.Boolean)
+
 
 
 @app.route('/')
@@ -18,9 +24,20 @@ def index():
 def item():
     return render_template('item-product.html')
 
-@app.route('/create')
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        item = Item(title=title, price=price)
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка"
+    else:
+        return render_template('create.html')
 
 @app.route('/signin')
 def signin():
